d4b750663ef74001f2c35830d402df380a46201a
Matthias Sehrbrock <matthias.sehrbrock@grandcentrix.net>
2021-04-10 14:10:48 +0200

    delete delivers no error on empty cache

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -71,6 +71,6 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 	
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-		fatalError("Must be implemented")
+		completion(nil)
 	}
 }
